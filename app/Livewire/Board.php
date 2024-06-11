0a0f34628005db871cd1976170b916c376353274
Ivo Vilar <ivo@grupovoz.dev.br>
2024-06-11 16:03:14 -0300

    backend: `list of companies`

--- app/Livewire/Board.php
+++ app/Livewire/Board.php
@@ -2,14 +2,27 @@
 
 namespace App\Livewire;
 
+use App\Models\Company;
+use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\View\View;
 use Livewire\Component;
+use Livewire\WithPagination;
 
+/** @property-read LengthAwarePaginator|Company[] $companies */
 class Board extends Component
 {
+    use WithPagination;
 
     public function render(): View
     {
-        return view('livewire.board');
+        $countCompanies = Company::query()->count();
+        return view('livewire.board', [
+            'totalCompanies' => $countCompanies,
+        ]);
+    }
+
+    public function companies(): LengthAwarePaginator
+    {
+        return Company::query()->paginate(10);
     }
 }
