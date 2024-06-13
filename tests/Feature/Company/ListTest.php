634edf6c17f0007f0eda6a4bfe38791f7ad8f93c
Ivo Vilar <ivo@grupovoz.dev.br>
2024-06-13 09:21:55 -0300

    backend: `should be able to list all companies`

--- tests/Feature/Company/ListTest.php
+++ tests/Feature/Company/ListTest.php
@@ -1,8 +1,23 @@
 <?php
 
+use App\Livewire\CompanyBoard;
+use App\Models\Company;
 use function Pest\Laravel\get;
 
 it('should be able to render component', function () {
     get(route('home'))
         ->assertOk();
+});
+
+test('should be able to list all companies', function () {
+    $companies = Company::factory()->count(10)->create();
+    $lw = Livewire::test(CompanyBoard::class);
+
+    $lw->assertSet('companies', function ($companies) {
+        expect($companies)->toHaveCount(10);
+        return true;
+    });
+    foreach ($companies as $company) {
+        $lw->assertSee($company->name);
+    }
 });
